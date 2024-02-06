87b95ad9c9ec1550ecf7f60035e7723cb518502b
Thomas Lindae <thomas@lindae.de>
2024-02-06 09:31:05 +0100

    fixed "Could not find hl_group" message appearing when it shouldn't

--- lua/isabelle-lsp.lua
+++ lua/isabelle-lsp.lua
@@ -57,38 +57,39 @@ local function apply_config(isabelle_path, vsplit)
     local output_window
     local output_buffer
 
+    -- setting false means "don't do any highlighting for this group"
     local hl_group_map = {
-        ['background_unprocessed1'] = nil,
-        ['background_running1'] = nil,
-        ['background_canceled'] = nil,
-        ['background_bad'] = nil,
-        ['background_intensify'] = nil,
+        ['background_unprocessed1'] = false,
+        ['background_running1'] = false,
+        ['background_canceled'] = false,
+        ['background_bad'] = false,
+        ['background_intensify'] = false,
         ['background_markdown_bullet1'] = 'markdownH1',
         ['background_markdown_bullet2'] = 'markdownH2',
         ['background_markdown_bullet3'] = 'markdownH3',
         ['background_markdown_bullet4'] = 'markdownH4',
-        ['foreground_quoted'] = nil,
+        ['foreground_quoted'] = false,
         ['text_main'] = 'Normal',
         ['text_quasi_keyword'] = 'Keyword',
         ['text_free'] = 'Function',
         ['text_bound'] = 'Identifier',
-        ['text_inner_numeral'] = nil,
+        ['text_inner_numeral'] = false,
         ['text_inner_quoted'] = 'String',
         ['text_comment1'] = 'Comment',
-        ['text_comment2'] = nil, -- seems to not exist in the LSP
-        ['text_comment3'] = nil,
-        ['text_dynamic'] = nil,
-        ['text_class_parameter'] = nil,
+        ['text_comment2'] = false, -- seems to not exist in the LSP
+        ['text_comment3'] = false,
+        ['text_dynamic'] = false,
+        ['text_class_parameter'] = false,
         ['text_antiquote'] = 'Comment',
         ['text_raw_text'] = 'Comment',
         ['text_plain_text'] = 'String',
-        ['text_overview_unprocessed'] = nil,
+        ['text_overview_unprocessed'] = false,
         ['text_overview_running'] = 'Todo',
-        ['text_overview_error'] = nil,
-        ['text_overview_warning'] = nil,
-        ['dotted_writeln'] = nil,
+        ['text_overview_error'] = false,
+        ['text_overview_warning'] = false,
+        ['dotted_writeln'] = false,
         ['dotted_warning'] = "DiagnosticWarn",
-        ['dotted_information'] = nil,
+        ['dotted_information'] = false,
         ['spell_checker'] = 'Underlined',
     }
 
@@ -209,19 +210,29 @@ local function apply_config(isabelle_path, vsplit)
 
                     local thy_buffer = find_buffer_by_uri(result.uri)
 
-                    if thy_buffer then
-                        for _, entry in ipairs(result.entries) do
-                            local hl_group = hl_group_map[entry.type]
-                            local id = hl_group_namespace_map[entry.type]
-                            if hl_group and id then
-                                vim.api.nvim_buf_clear_namespace(thy_buffer, id, 0, -1)
-                                decorator(hl_group, entry.content, id, thy_buffer)
-                            else
-                                vim.notify("Could not find hl_group " .. entry.type .. ".")
-                            end
-                        end
-                    else
+                    if not thy_buffer then
                         vim.notify("Could not find buffer for " .. result.uri .. ".")
+                        return
+                    end
+
+                    for _, entry in ipairs(result.entries) do
+                        local id = hl_group_namespace_map[entry.type]
+                        local hl_group = hl_group_map[entry.type]
+
+                        -- if id is nil, it means the hl_group_map doesn't know about this group
+                        if not id then
+                            -- in particular, hl_group is nil here too
+                            vim.notify("Could not find hl_group " .. entry.type .. ".")
+                            goto continue
+                        end
+
+                        -- if hl_group is false, it just means there is no highlighting done for this group
+                        if not hl_group then goto continue end
+
+                        vim.api.nvim_buf_clear_namespace(thy_buffer, id, 0, -1)
+                        decorator(hl_group, entry.content, id, thy_buffer)
+
+                        ::continue::
                     end
                 end,
             },
